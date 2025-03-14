0bccaa6d6ff810de01de90c8f62e43f37037e997
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-03-14 15:09:17 +0600

    Add section title component

--- src/components/RecentPostSection/RecentPostSection.jsx
+++ src/components/RecentPostSection/RecentPostSection.jsx
@@ -1,10 +1,13 @@
 import "./recent-post.scss";
+import SectionTitle from "../SectionTitle/SectionTitle";
 
 export default function RecentPostSection() {
   return (
     <section className="recent-post">
       <div className="container">
-        <div className="recent-post-top"></div>
+        <div className="recent-post-top">
+          <SectionTitle>Recent Posts</SectionTitle>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,5 @@
+import "./section-title.scss";
+
+export default function SectionTitle({ children }) {
+  return <h2 className="section-title">{children}</h2>;
+}
